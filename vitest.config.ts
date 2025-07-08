fc4e202d688a1e0a726c897fe0a8efa2020d6203
Ben Thompson <ben.thompson@futuresecure.ai>
2025-07-08 10:15:26 +1000

    Shakedown fixes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        environment: "node",
-        watch: false,
-        include: ["src/**/*.test.ts"],
-    },
+	test: {
+		environment: "node",
+		watch: false,
+		include: ["src/**/*.test.ts"],
+	},
 });
